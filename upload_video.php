64fa80838a9c2e183430afdae68c87d2ba916eac
Matthew Heick <github@heick.email>
2024-11-02 15:58:05 -0400

    lots of cleaner dies, grabbing upload Location: and getting ready to actually send data

--- upload_video.php
+++ upload_video.php
@@ -2,11 +2,11 @@
 <?php
 /* Create project API key from https://console.cloud.google.com/apis/credentials?project=<project name>, save it as api_key.txt */
 if (!file_exists('api_key.txt')) {
-	die('api_key.txt missing. create one and save it as api_key.txt');
+	die("api_key.txt missing. create one and save it as api_key.txt\n");
 }
 $apikey = trim(file_get_contents('api_key.txt'));
 if (strlen($apikey) == 0) {
-	die('api_key.txt is empty or bad. make sure it exists as the only data in the file');
+	die("api_key.txt is empty or bad. make sure it exists as the only data in the file\n");
 }
 
 $o = getopt('c:f:t:p:d:', ['code:', 'file:', 'title:', 'priv:', 'desc:']);
@@ -25,16 +25,16 @@ function print_help() {
 
 /* client_secrets.json, bread and butter */
 if (!file_exists('client_secrets.json')) {
-	die('client_secrets.json missing');
+	die("client_secrets.json missing\n");
 }
 $client_secrets = json_decode(file_get_contents('client_secrets.json'), true);
-if (!isset($client_secrets['web'])) { die('client_secrets.json, no web key'); }
-if (!isset($client_secrets['web']['auth_uri'])) { die('client_secrets.json, no auth_uri key'); }
-if (!isset($client_secrets['web']['token_uri'])) { die('client_secrets.json, no token_uri key'); }
-if (!isset($client_secrets['web']['client_id'])) { die('client_secrets.json, no client_id key'); }
-if (!isset($client_secrets['web']['client_secret'])) { die('client_secrets.json, no client_secret key'); }
-if (!isset($client_secrets['web']['redirect_uris'])) { die('client_secrets.json, no redirect_uris key'); }
-if (count($client_secrets['web']['redirect_uris']) == 0) { die('client_secrets.json, redirect_uris are empty'); }
+if (!isset($client_secrets['web'])) { die("client_secrets.json, no web key\n"); }
+if (!isset($client_secrets['web']['auth_uri'])) { die("client_secrets.json, no auth_uri key\n"); }
+if (!isset($client_secrets['web']['token_uri'])) { die("client_secrets.json, no token_uri key\n"); }
+if (!isset($client_secrets['web']['client_id'])) { die("client_secrets.json, no client_id key\n"); }
+if (!isset($client_secrets['web']['client_secret'])) { die("client_secrets.json, no client_secret key\n"); }
+if (!isset($client_secrets['web']['redirect_uris'])) { die("client_secrets.json, no redirect_uris key\n"); }
+if (count($client_secrets['web']['redirect_uris']) == 0) { die("client_secrets.json, redirect_uris are empty\n"); }
 
 /* If we're passing in a code we need to authorize the app */
 if (isset($o['code'])) {
@@ -57,7 +57,8 @@ if (isset($o['code'])) {
 	]);
 	$data = curl_exec($ch);
 	file_put_contents('oauth.json', $data);
-	die();
+	curl_close($ch);
+	die("I've written the oauth.json file. Uploading should be able to commence\n");
 }
 
 /* oauth.json, the stuff we need to upload. authenticate if we don't have the file */
@@ -70,8 +71,7 @@ if (!file_exists('oauth.json')) {
 		'access_type' => 'offline',
 	]);
 	$url = $client_secrets['web']['auth_uri'] . '?' . $params;
-	echo "Go to the following URL:\n\n$url\n\nOnce you've authorized provide the code here as\nupload_video.php --code \"code\"\n";
-	die();
+	die("Go to the following URL:\n\n$url\n\nOnce you've authorized provide the code here as\nupload_video.php --code \"code\"\n");
 }
 
 /* Load up our file. Do maintenance */
@@ -81,9 +81,8 @@ if (isset($oauth['error'])) {
 	if (isset($oauth['error_description'])) {
 		echo ': ' . $oauth['error_description'];
 	}
-	echo "\n";
 	unlink('oauth.json');
-	die();
+	die("\n");
 }
 /* Our custom field of when we got this access */
 if (!isset($oauth['issued'])) {
@@ -94,6 +93,7 @@ if (!isset($oauth['issued'])) {
 }
 /* Check to see if we need to run the refresh token flow */
 if ($oauth['expires'] < time()) {
+	echo "token expired. renewing...";
 	/* Must refresh our token */
 	$params = http_build_query([
 		'client_id' => $client_secrets['web']['client_id'],
@@ -113,26 +113,24 @@ if ($oauth['expires'] < time()) {
 	/* Check to make sure we got everything */
 	$refresh = json_decode($data, true);
 	if (!(isset($refresh['access_token']) || isset($refresh['expires_in']))) {
-		echo "error refreshing access token: " . var_export($refresh, true) . "\n";
-		die();
+		die("error refreshing access token: " . var_export($refresh, true) . "\n");
 	}
 	$oauth['issued'] = time();
 	$oauth['expires'] = intval($oauth['issued']) + intval($refresh['expires_in']);
 	$oauth['access_token'] = $refresh['access_token'];
 	file_put_contents('oauth.json', json_encode($oauth));
 	curl_close($ch);
+	echo "ok\n";
 }
 
 /* At this point we've done everything oauth-related and have a good access token */
 if (!isset($o['file'])) {
-	echo "--file not provided.\n";
 	print_help();
-	die();
+	die("\n--file not provided.\n");
 }
 if (!file_exists($o['file'])) {
-	echo "file " . $o['file'] . " not found.\n";
 	print_help();
-	die();
+	die("\nfile " . $o['file'] . " not found.\n");
 }
 
 /* Lets get this file uploaded and start taking on quota */
@@ -157,7 +155,6 @@ curl_setopt_array($ch, [
 		'X-Upload-Content-Type: video/mp4',
 	],
 	/* https://developers.google.com/youtube/v3/guides/using_resumable_upload_protocol##Start_Resumable_Session */
-	//CURLOPT_URL => 'https://www.googleapis.com/upload/youtube/v3/videos?uploadType=resumable&part=snippet&part=status&part=id&part=contentDetails&key=' . urlencode($apikey),
 	CURLOPT_URL => 'https://www.googleapis.com/upload/youtube/v3/videos?uploadType=resumable&part=' . urlencode('snippet,status,id,contentDetails') . '&key=' . urlencode($apikey),
 	CURLOPT_POST => true,
 	CURLOPT_HEADER => true,
@@ -165,15 +162,28 @@ curl_setopt_array($ch, [
 	CURLOPT_POSTFIELDS => $content,
 ]);
 $data = curl_exec($ch);
-
-/* See if this is a "Video" resource */
-$res = json_decode($data, true);
-echo var_export($data, true);
-echo var_export($res, true);
 curl_close($ch);
 
-/* We should have a Location: (.*) to parse out of the header if we detect a 200 */
-die();
+/* Resumable should give us a 200 with a Location: to upload to */
+if (preg_match('|HTTP/1.1\s(\d+)\s|', $data, $s) !== 1) {
+	var_export($data);
+	die("\nExpecting 200 from google. didn't see it\n");
+}
+$status = $s[1] ?? 'unset';
+if ($status != '200') {
+	var_export($data);
+	die("\nExpecting 200 from google. parsed '$status' instead\n");
+}
+if (preg_match('|Location:\s(.*)|', $data, $m) !== 1) {
+	var_export($data);
+	die("\nExpecting Location: from google. didn't see it\n");
+}
+$location = $m[1] ?? 'unset';
+if ($location == 'unset') {
+	var_export($data);
+	die("\nExpecting Location: from google. got nothing from it\n");
+}
+
 /* Run a PUT with the file */
 $ch = curl_init();
 $file_contents = file_get_contents($o['file']);
@@ -184,10 +194,15 @@ curl_setopt_array($ch, [
 		'Content-Length: ' . $content_length,
 		'Content-Type: video/mp4',
 	],
-	CURLOPT_URL => 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=status&part=id&part=contentDetails',
+	CURLOPT_URL => $location,
 	CURLOPT_CUSTOMREQUEST => 'PUT',
 	CURLOPT_HEADER => true,
 	CURLOPT_RETURNTRANSFER => true,
 	CURLOPT_POSTFIELDS => $file_contents,
 ]);
 $data = curl_exec($ch);
+
+/* Lets dump this response so we know what's going on */
+echo "File uploaded. Response is the following:\n";
+var_export($data);
+echo "\n";
